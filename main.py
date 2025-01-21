6777ef2f8c080a9fae139c78e13bb0c32ef4ed64
bf01863644 <bsd_unix@naver.com>
2025-01-21 20:56:08 +0900

    port 지정:8000

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.routes import proxy
 from src.middleware.auth import AuthMiddleware
+import uvicorn
 
 app = FastAPI()
 
@@ -16,3 +17,6 @@ app.add_middleware(
 app.add_middleware(AuthMiddleware)
 
 app.include_router(proxy.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)

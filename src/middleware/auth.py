fc849c4c36fa60ae03fd928a48eabad7adf87f18
myeongsoyi <myeongsoyi@gmail.com>
2025-02-17 16:16:19 +0900

    feat: redis default 삭제

--- src/middleware/auth.py
+++ src/middleware/auth.py
@@ -20,8 +20,8 @@ class AuthMiddleware(BaseHTTPMiddleware):
         super().__init__(app)
         try:
             self.redis_client = Redis(
-                host=os.getenv('REDIS_HOST', '127.0.0.1'),
-                port=int(os.getenv('REDIS_PORT', 6379)),
+                host=os.getenv('REDIS_HOST'),
+                port=int(os.getenv('REDIS_PORT')),
                 db=0,
                 decode_responses=True
             )
